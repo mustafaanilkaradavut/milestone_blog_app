f6b04a59c5e9021805f359ffc3c92e7d4f60bf74
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-06-21 04:47:12 +0100

    Templete My Blog App

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+//__ Added main css file.
+// @tailwind base;
+// @tailwind components;
+// @tailwind utilities;
